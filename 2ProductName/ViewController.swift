836e390d17a38ebd08a80baaf790d8ded9bf8ab2
Stephanie Ramos <ramos.n.stephanie@outlook.com>
2019-10-29 11:20:35 -0400

    Initial Commit

---
+++ 2ProductName/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2ProductName
+//
+//  Created by Stephanie Ramos on 10/29/19.
+//  Copyright © 2019 Stephanie N. Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
